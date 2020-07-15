8dc314bdb68b7335061e9481e044568e093cc83c
Sergio Nunez <sergionunez@MacBook-Pro.local>
2020-07-15 17:27:46 -0500

    Termino de proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
+import { AppRoutingModule } from './app.routes';
+
+//Servicios
+import { HeroesService } from './servicios/heroes.service';
+
+
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    BuscadorComponent,
+    HeroeTarjetaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html',
+  styleUrls: ['./buscador.component.css']
+})
+export class BuscadorComponent implements OnInit {
+
+  heroes: any[] = [];
+  termino: string;
+
+  constructor(private activatedroute: ActivatedRoute,
+    private _heroesService: HeroesService) { }
+
+  ngOnInit(): void {
+    this.activatedroute.params.subscribe( params =>{
+      // console.log(params['termino']);
+      this.termino = params['termino'];
+      this.heroes = this._heroesService.buscarHeroes(params['termino']);
+      console.log(this.heroes);
+    });
+  }
+
+}
